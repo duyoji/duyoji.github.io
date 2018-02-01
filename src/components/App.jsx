86bb8753f17263f6714fd0c4fa880d3b2ebae256
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2018-02-01 23:17:28 +0900

    Added react-ga

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,16 +5,17 @@ import HomePage from './HomePage';
 import ProfilePage from './ProfilePage';
 import CareerPage from './CareerPage';
 import ContactPage from './ContactPage';
+import withGATracker from '../containers/withGATracker';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <Route exact={true} path="/" component={HomePage} />
-        <Route exact={true} path="/profile" component={ProfilePage} />
-        <Route exact={true} path="/career" component={CareerPage} />
-        <Route exact={true} path="/contact" component={ContactPage} />
+        <Route exact={true} path="/" component={withGATracker(HomePage)} />
+        <Route exact={true} path="/profile" component={withGATracker(ProfilePage)} />
+        <Route exact={true} path="/career" component={withGATracker(CareerPage)} />
+        <Route exact={true} path="/contact" component={withGATracker(ContactPage)} />
       </div>
     );
   }
